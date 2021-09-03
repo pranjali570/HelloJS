8c6673d104563a92d956b4657fbff4f655000967
pranjali570 <pranjali.nagal@gmail.com>
2021-09-03 20:17:10 +0530

    operators

--- app.js
+++ app.js
@@ -1,6 +1,119 @@
-//constants
+// Arithmetic operators
 
-const My_Name = "Pranjali"
-My_Name = "Pranjali Agrawal"
+const a = 5
+const b = 13
 
-console.log(My_Name)
+console.log(a + b)
+
+console.log(a - b)
+
+console.log(a * b)
+
+console.log(a / b)
+
+console.log(a ** b)
+
+console.log(a % b)
+
+console.log("--------------------------------------------")
+
+// Increment Operator
+
+let c = 0
+
+c++
+console.log(c)
+
+++c
+console.log(c)
+
+console.log("--------------------------------------------")
+
+
+// Compound Assignment
+
+let d = 5
+
+d += 7
+console.log(d)
+
+d -= 7
+console.log(d)
+
+d *= 10
+console.log(d)
+
+d /= 10
+console.log(d)
+
+d **= 3
+console.log(d)
+
+d %= 9
+console.log(d)
+
+console.log("--------------------------------------------")
+
+//String Concatenation
+
+let str1 = "Day "
+let str2 = " Sixteen"
+
+console.log(str1 + str2)
+
+console.log("--------------------------------------------")
+
+// Comparison Operator
+
+const num1 = 20
+const num2 = 34
+const num3 = 20
+const str = "hello"
+
+console.log("20 > 34", num1 > num2)
+
+console.log("20 < 34", num1 < num2)
+
+console.log("20 >= 34", num1 >= num2)
+
+console.log("20 <= 34", num1 <= num2)
+
+console.log("20 == 34", num1 == num2)
+
+console.log("20 == 20", num1 == num3)
+
+console.log("20 == hello", num1 == str)
+
+console.log("20 === hello", num1 === str)
+
+console.log("20 === 34", num1 === num2)
+
+console.log("20 === 20", num1 === num3)
+
+console.log("20 != 34", num1 != num2)
+
+console.log("20 != hello", num1 != str)
+
+console.log("hello == HELLO", "hello" == "HELLO")
+
+console.log("hello === HELLO", "hello" === "HELLO")
+
+console.log("--------------------------------------------")
+
+// Logical Operators
+
+console.log("AND")
+console.log("false && false", false && false)
+console.log("false && true", false && true)
+console.log("true && false", true && false)
+console.log("true && true", true && true)
+
+console.log("OR")
+console.log("false || false", false || false)
+console.log("false || true", false || true)
+console.log("true || false", true || false)
+console.log("true || true", true || true)
+
+console.log("--------------------------------------------")
+
+console.log("--------------------------------------------")
