0de896575f43530bc0255810cac7409bc64dfa41
pranjali570 <pranjali.nagal@gmail.com>
2021-09-06 19:33:36 +0530

    Rock Paper Scissor

--- app.js
+++ app.js
@@ -6,15 +6,33 @@ const board = document.querySelector("#board")
 
 const handler = (e) => {
     const action = e.target.id
-    if (action === 'rock') {
-        board.innerHTML = "Rock crushes scissors"
-    } else if (action === 'paper') {
-        board.innerHTML = "Paper traps Rock"
-    } else if (action === 'scissor') {
-        board.innerHTML = "Scissor cuts paper"
-    } else {
-        board.innerHTML = "Click something"
+    // if (action === 'rock') {
+    //     board.innerHTML = "Rock crushes scissors"
+    // } else if (action === 'paper') {
+    //     board.innerHTML = "Paper traps Rock"
+    // } else if (action === 'scissor') {
+    //     board.innerHTML = "Scissor cuts paper"
+    // } else {
+    //     board.innerHTML = "Click something"
+    // }
+
+    switch (action) {
+        case "rock":
+            board.innerHTML = "Rock crushes scissors"
+            break
+
+        case 'paper':
+            board.innerHTML = "Paper traps Rock"
+            break
+
+        case 'scissor':
+            board.innerHTML = "Scissor cuts paper"
+            break
+            
+        default:
+            board.innerHTML = "Click something"
     }
+
 }
 
 
