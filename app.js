cfc570b94c39bbcd3f9358f133126468818d6cbe
pranjali570 <pranjali.nagal@gmail.com>
2021-09-06 19:28:44 +0530

    control statements

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
-// Coercion
+const rock = document.querySelector('#rock')
+const paper = document.querySelector('#paper')
+const scissor = document.querySelector('#scissor')
+const board = document.querySelector("#board")
 
-let My_Name = "Pranjali"
-My_Name = "Pranjali Agrawal"
 
-console.log(My_Name.toUpperCase())
+const handler = (e) => {
+    const action = e.target.id
+    if (action === 'rock') {
+        board.innerHTML = "Rock crushes scissors"
+    } else if (action === 'paper') {
+        board.innerHTML = "Paper traps Rock"
+    } else if (action === 'scissor') {
+        board.innerHTML = "Scissor cuts paper"
+    } else {
+        board.innerHTML = "Click something"
+    }
+}
+
+
+
+rock.addEventListener('click', handler)
+paper.addEventListener('click', handler)
+scissor.addEventListener('click', handler)
 
