4886d816594cb868e3418d22eb67df1ef99a5da1
pranjali570 <pranjali.nagal@gmail.com>
2021-09-07 18:07:23 +0530

    array methods

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
-let iteration = 1
+const balls = document.getElementsByClassName('ball')
+const body = document.querySelector('body')
 
-while(iteration <= 10)
+const arr = [1, 2, 3, 4, 5]
+console.log(arr.push(6))
+
+let i = 7
+while(arr.length < 10)
 {
-    console.log(iteration)
-    iteration++
+    arr.push(i)
+    i++;
 }
+
+console.log(arr)
+console.log(arr.length)
+console.log(arr.includes(10))
+console.log(arr.indexOf(5))
+arr.unshift(0)
+console.log(arr)
+arr.shift()
+console.log(arr)
+
+arr.splice(6, 1)
+arr.forEach((element, iteration) => {
+    console.log(element + '-->' + iteration)
+})
+
+
