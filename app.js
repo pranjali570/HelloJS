05f8d60992e2f2869d74e8f40b6f7a977eaecfab
pranjali570 <pranjali.nagal@gmail.com>
2021-09-07 18:32:52 +0530

    template-string

--- app.js
+++ app.js
@@ -1,43 +1,17 @@
-const rock = document.querySelector('#rock')
-const paper = document.querySelector('#paper')
-const scissor = document.querySelector('#scissor')
-const board = document.querySelector("#board")
+// const balls = document.getElementsByClassName("ball")
+const balls = document.querySelectorAll(".ball")
+const body = document.querySelector("body")
 
 
-const handler = (e) => {
-    const action = e.target.id
-    // if (action === 'rock') {
-    //     board.innerHTML = "Rock crushes scissors"
-    // } else if (action === 'paper') {
-    //     board.innerHTML = "Paper traps Rock"
-    // } else if (action === 'scissor') {
-    //     board.innerHTML = "Scissor cuts paper"
-    // } else {
-    //     board.innerHTML = "Click something"
-    // }
-
-    switch (action) {
-        case "rock":
-            board.innerHTML = "Rock crushes scissors"
-            break
-
-        case 'paper':
-            board.innerHTML = "Paper traps Rock"
-            break
-
-        case 'scissor':
-            board.innerHTML = "Scissor cuts paper"
-            break
-            
-        default:
-            board.innerHTML = "Click something"
-    }
-
-}
+balls.forEach((ball, iteration) => {
+    const value = (iteration + 1) * 50
+    ball.style = `transform: translateY(${value}px)`
+})
 
 
+let age = 19
+let Name = "Pranjali"
 
-rock.addEventListener('click', handler)
-paper.addEventListener('click', handler)
-scissor.addEventListener('click', handler)
+console.log('Your age is ' + age)
+console.log(`Hi ${Name} Your age is ${age}`)
 
