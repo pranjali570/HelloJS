2a6041933bb315b01c7f3ce0cea4a44d55c944ed
pranjali570 <pranjali.nagal@gmail.com>
2021-09-08 20:10:03 +0530

    ES6 destructuring

--- app.js
+++ app.js
@@ -1,7 +1,47 @@
-// Coercion
+const arr = [1, 2, 3, 4]
+console.log(arr[2])
 
-let My_Name = "Pranjali"
-My_Name = "Pranjali Agrawal"
+// ES6 Destructuring
+const [one, two, three, four, five] = [1, 2, 3, 4]
+console.log(three)
+console.log(four)
+console.log(five)
 
-console.log(My_Name.toUpperCase())
+function getscores(){
+    return [90, 100]
+}
 
+let [x, y, z] = getscores()
+
+console.log(x)
+console.log(y)
+console.log(z)
+
+const obj = {
+    Name: "Nikhil Agarwal",
+    email: "nikhilagarwaliitkgp@gmail.com",
+    age: 20
+}
+
+console.log(obj.email)
+
+let {dum, Name, age, email} = obj
+console.log(email)
+console.log(age)
+console.log(Name)
+console.log(dum)
+
+let person = {
+    firstname: "Pranjali",
+    lastname: "Agrawal"
+}
+
+let {firstname = "", middlename = "", lastname = ""} = person
+console.log(firstname, middlename, lastname)
+
+
+let nums = [[1, 2, 3],[4, 5, 6]]
+// let [num1, num2] = nums
+// console.log(num1)
+let [[num11, num12, num13], [num21, num22, num23]] = nums
+console.log(num11)
