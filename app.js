c904311bd53d8c85997c2de491a8cbc1fcdfe463
pranjali570 <pranjali.nagal@gmail.com>
2021-09-11 19:53:27 +0530

    async & await

--- app.js
+++ app.js
@@ -4,18 +4,57 @@ let concert = false;
 let attendConcert = new Promise(function (resolve, reject) {
     setTimeout(() => {
         if (concert) {
-            resolve("BOB ATTENDED THE CONCERT")
+            resolve("BOB ATTENDED THE CONCERT");
         }
         else {
-            reject("BOB FAILED TO ATTEND THE CONCERT")
+            reject("BOB FAILED TO ATTEND THE CONCERT");
         }
-    }, 2000)
+    }, 2000);
 });
 
-console.log(attendConcert)
+// let attendConcert = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         if (concert) {
+//             resolve("BOB ATTENDED THE CONCERT");
+//         }
+//         else {
+//             reject("BOB FAILED TO ATTEND THE CONCERT");
+//         }
+//     }, 2000);
+// });
+
+// console.log(attendConcert)
 console.log(attendConcert)
 
-// attendConcert.then((data) => console.log(data))
-// attendConcert.catch((error) => console.log(error))
+// attendConcert.then((data)=>console.log(data));
 
-attendConcert.then((data) => console.log(data)).catch((error) => console.log(error))
+// attendConcert.catch((error)=>console.log(error));
+
+// attendConcert.then((data)=>console.log(data)).catch((error)=>console.log(error));
+
+
+// async function asyfuncname() {
+//   try {
+//     let result = await attendConcert;
+//     console.log(result)
+//   }
+//   catch(e) {
+//     console.log(e)
+//   }
+// }
+// asyfuncname()
+
+let asyfuncname = async() => {
+  try {
+    let result = await attendConcert;
+    console.log(result)
+  }
+  catch(e) {
+    console.log(e)
+  }
+}
+asyfuncname()
+
+console.log("HI, I WON'T WAIT FOR ANYONE")
+
+console.log("HI, I AM DYNING, I DON'T WANT TO WAIT")
